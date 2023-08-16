6fdaf225424016e5d58adf2f88707d0f058fbeb2
LukanONE <lucasgabriel.silveira10@gmail.com>
2023-08-15 22:01:39 -0300

    ajeitando o que esqueci das datas

--- atividadesSiteSoftex/nomeIdade.js
+++ atividadesSiteSoftex/nomeIdade.js
@@ -5,7 +5,6 @@ do usuário e a idade que completou, ou completará, no ano atual (2022).
 Caso o usuário não digite um número ou apareça um inválido no campo do ano, 
 o sistema informará o erro e continuará perguntando até que um valor correto seja preenchido.
 */
-
 const readline = require('readline-sync')
 
 
@@ -13,8 +12,8 @@ while(true){
     try{
         let nome = readline.question("informe seu nome completo: ")
         let dataNascimento = readline.questionInt("digite o ano do seu nascimento: ")
-        if(isNaN(dataNascimento) == true){
-            console.log(`${dataNascimento} não é um numero, favor digite um numero`);
+        if(isNaN(dataNascimento) == true || dataNascimento < 1922 || dataNascimento > 2022){
+            console.log(`${dataNascimento} não é um numero ou é menor que 1922 ou maior que 2022, favor digite um numero`);
         } else{
             let idade = 2023 - dataNascimento
             console.log(`Seu nome é ${nome}, sua idade é de ${idade} anos.`);
