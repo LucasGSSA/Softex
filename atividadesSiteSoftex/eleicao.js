aab0166273e955f5e3254793bd4cf2eff97e5900
LukanONE <lucasgabriel.silveira10@gmail.com>
2023-08-23 15:55:56 -0300

    Atividade do site eleicoes

---
+++ atividadesSiteSoftex/eleicao.js
@@ -0,0 +1,62 @@
+// Desenvolva um código que simule uma eleição com três candidatos.
+// - candidato_X => 889
+// - candidato_Y => 847
+// - candidato_Z => 515
+// - branco => 0
+
+// O código deve perguntar se deseja finalizar a votação depois do voto. 
+// Caso o número do voto não corresponda a nenhum candidato ou seja branco, 
+// ele deve ser tratado como nulo. Se for inserido um texto ao invés de número, 
+// o código deve retornar uma mensagem para votar novamente.
+
+// Quando a votação for finalizada, o código deverá mostrar o vencedor, 
+// aquele com o maior número de votos e, também, a quantidade de 
+// votos de cada candidato, os brancos e nulos. 
+
+const readline = require("readline-sync");
+
+let candidato_X = 0;
+let candidato_Y = 0;
+let candidato_Z = 0;
+let brancosOuNulos = 0;
+
+let votacao = true;
+while(votacao){
+    let perguntaVoto = readline.questionInt("Digite o numero que voce quer votar: ");
+    switch(perguntaVoto){
+        case 889:
+            candidato_X += 1;
+            break;
+        case 847:
+            candidato_Y += 1;
+            break;
+        case 515:
+            candidato_Z += 1;
+            break;
+        default:
+            brancosOuNulos += 1;
+            break;
+    };
+
+    let repeticao = true;
+    while(repeticao){
+        let perguntaFimVoto = readline.question(`deseja finalizar as votacoes? Digite "sim" ou "nao": `);
+        switch(perguntaFimVoto){
+            case 'sim':
+                votacao = false;
+                repeticao = false;
+                break;
+            case 'nao':
+                repeticao = false;
+                break;
+            default:
+                console.log(`Nao entendi, digite apenas "sim" ou "nao"`);
+            break;               
+        }   
+    }
+}
+
+console.log(`Votos para Candidato X = ${candidato_X}.`);
+console.log(`Votos para Candidato Y = ${candidato_Y}.`);
+console.log(`Votos para Candidato Z = ${candidato_Z}.`);
+console.log(`Votos Brancos ou Nulos = ${brancosOuNulos}.`);
