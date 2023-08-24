33407227898032616a8f2456286c9ef76fee2bfa
Lucas <lucasgabriel.silveira10@gmail.com>
2023-08-24 16:06:31 -0300

    array

---
+++ atividadesSiteSoftex/array.js
@@ -0,0 +1,12 @@
+let nomes = ['lucas', 'joao', 'julia', 'clara', 'maria','josé', 'pedro', 'miguel','guilherme','luiza'];
+let idades = [18,18,19,17,19,20,30,29,20,39];
+let cor = ['azul','verde','vermelho','roxo','cinza','amarelho','laranja','marrom','ciano','rosa'];
+
+console.log(nomes);
+console.log(idades);
+console.log(cor);
+
+cor.splice(2,1,'branco');
+idades.splice(1,1,20)
+console.log(cor);
+console.log(idades);
