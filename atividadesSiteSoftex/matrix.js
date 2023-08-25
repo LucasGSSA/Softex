ca087b0e876f391b646ccde590adaba9b584129b
LukanONE <lucasgabriel.silveira10@gmail.com>
2023-08-24 21:26:22 -0300

    adicionando nova atividade do site

---
+++ atividadesSiteSoftex/matrix.js
@@ -0,0 +1,7 @@
+let matrix = [
+    ['Animal: Cachorro', 'Nome: Willy','Raça: Puddle', 'Idade: 9 Anos'],
+    ['Animal: Gato','Nome: Nina','Raça: Brasileiro', 'Idade: 4'],
+    ['Animal: Peixe','Nome: Beto','Raça: Beta', 'Idade: 2'],
+];
+
+console.log(matrix[0][1]);
