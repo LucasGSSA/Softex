037b912c951691a136cc1bba9a1c8e59d614a8cd
LukanONE <lucasgabriel.silveira10@gmail.com>
2023-09-12 19:25:22 -0300

    atividade tipos de dados

---
+++ atividadesSiteSoftex/tiposDados.js
@@ -0,0 +1,24 @@
+let boleaan;
+let undefined;
+let number;
+let string;
+let nulo;
+let simbol;
+let bignumber;
+
+console.log(boleaan, undefined, number, string, nulo, simbol, bignumber);
+
+boleaan = true;
+number = 7;
+string = "texto";
+nulo = null;
+simbol = Symbol();
+bignumber = 123123123213123123123131312312313n;
+
+console.log(typeof(boleaan));
+console.log(typeof(undefined));
+console.log(typeof(number));
+console.log(typeof(string));
+console.log(typeof(nulo));
+console.log(typeof(simbol));
+console.log(typeof(bignumber));
