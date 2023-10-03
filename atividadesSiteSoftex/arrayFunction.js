5dbe49f8c961f8a21a667a4f8e5a92ea96779051
Lucas Silveira <130493122+LucasGSSA@users.noreply.github.com>
2023-10-03 17:17:30 +0000

    trocando de pc e fazendo as atividadwes

---
+++ atividadesSiteSoftex/arrayFunction.js
@@ -0,0 +1,13 @@
+function normal(){
+    return "Olá, pessoa"
+};
+
+function parametros(nome){
+    return `Olá ${nome}`
+};
+
+const arquinho = (nome, nota1, nota2) => {
+    return `olá ${nome}, média: ${(nota1+nota2)/2}`
+};
+
+console.log(arquinho("lucas", 7,8));
